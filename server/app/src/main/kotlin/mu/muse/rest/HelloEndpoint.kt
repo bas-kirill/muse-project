46fbeac21c202459b7c390479efea7dcd5fb7bd2
skywalker <baskirill.an@gmail.com>
2024-07-18 11:25:24 +0300

    feat(infra): add `ktlint` linter to project and refactor code according it
    `ktlint` was addeed to gradle build

--- server/app/src/main/kotlin/mu/muse/rest/HelloEndpoint.kt
+++ server/app/src/main/kotlin/mu/muse/rest/HelloEndpoint.kt
@@ -3,7 +3,6 @@ package mu.muse.rest
 import org.springframework.web.bind.annotation.GetMapping
 
 class HelloEndpoint {
-
     @GetMapping("/hello")
     fun hello(): String {
         return "Hello, World!"
