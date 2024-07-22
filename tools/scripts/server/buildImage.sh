d3a81b085f20fa4913503a60bda0a3c9d1910315
Kirill <baskirill.an@gmail.com>
2024-07-22 22:15:50 +0300

    feat(frontend): dockerize frontend, update server scripts and CI/CD yamls

---
+++ tools/scripts/server/buildImage.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+rootDir="$currentDir/../../../"
+
+imageTag=$1
+
+if [ -z "$1" ]
+  then
+    echo 'No image tag provided. Latest will be used.'
+    imageTag=latest
+fi
+
+imageFullName=muse-server:$imageTag
+
+echo [MUSE SERVER STARTING] building "$imageFullName"...
+
+echo [MUSE SERVER] remove old image "$imageFullName"...
+
+# Check if the image exists before attempting to remove it
+if docker images -q "$imageFullName" &> /dev/null; then
+  echo [MUSE SERVER] remove old image "$imageFullName"...
+  docker rmi -f "$imageFullName"
+fi
+
+echo [MUSE SERVER] creating docker image "$imageFullName"...
+(docker build -f "${rootDir}/server/Dockerfile" -t "$imageFullName" "$rootDir")
+
+echo [MUSE SERVER FINISHED] image "$imageFullName" has been built
