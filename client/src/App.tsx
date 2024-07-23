48f453724012e973145d539d60c6c893a2d7a2fe
Kirill <baskirill.an@gmail.com>
2024-07-23 15:54:42 +0300

    feat(home): add home page (header and footer too)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Home from "./ui/Home/Home";
+import NotFound from "./ui/NotFound/NotFound";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div id="app">
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<NotFound/>}/>
+        </Routes>
     </div>
-  );
-}
+);
+
 
 export default App;
