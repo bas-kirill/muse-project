f5ccc4416e6a57f7029a2ffcd5742555ab8f77ad
Kirill <baskirill.an@gmail.com>
2024-07-23 23:20:37 +0300

    feat(get-profile): add `/api/profile` endpoint

--- server/app/src/main/kotlin/mu/muse/application/muse/RestConfiguration.kt
+++ server/app/src/main/kotlin/mu/muse/application/muse/RestConfiguration.kt
@@ -2,7 +2,9 @@ package mu.muse.application.muse
 
 import mu.muse.rest.HelloEndpoint
 import mu.muse.rest.BasicLoginEndpoint
+import mu.muse.rest.GetProfileEndpoint
 import mu.muse.usecase.BasicLogin
+import mu.muse.usecase.GetProfile
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -14,4 +16,7 @@ class RestConfiguration {
 
     @Bean
     fun loginEndpoint(basicLogin: BasicLogin) = BasicLoginEndpoint(basicLogin)
+
+    @Bean
+    fun getProfileEndpoint(getProfile: GetProfile) = GetProfileEndpoint(getProfile)
 }
---
+++ server/app/src/main/kotlin/mu/muse/usecase/scenario/GetProfileUseCase.kt
@@ -0,0 +1,15 @@
+package mu.muse.usecase.scenario
+
+import mu.muse.domain.Username
+import mu.muse.usecase.GetProfile
+import mu.muse.usecase.ShowProfileError
+import mu.muse.usecase.access.UserExtractor
+import mu.muse.usecase.dto.ProfileDetails
+
+class GetProfileUseCase(private val userExtractor: UserExtractor) : GetProfile {
+
+    override fun execute(username: Username): ProfileDetails {
+        val user = userExtractor.findByUsername(username) ?: throw ShowProfileError.UserNotFound(username)
+        return ProfileDetails.from(user)
+    }
+}
