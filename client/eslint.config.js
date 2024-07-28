99dbbf81e2cc957d31212cd3e78c0efd33dd93ac
skywalker <baskirill.an@gmail.com>
2024-07-28 16:28:12 +0300

    fix(eslint): try to fix error at CI

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,13 +1,10 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import { browser } from "globals";
+import { configs } from "@eslint/js";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: browser } },
+  configs.recommended,
+  ...configs.recommended,
+  configs.flat.recommended,
 ];
