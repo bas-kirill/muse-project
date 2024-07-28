5e63cbcd6fd97f414198775e6b06dd1feec59d1d
Kirill <baskirill.an@gmail.com>
2024-07-28 21:28:32 +0300

    feat(frontend): add button to delete item with mocked HTTP response

---
+++ client/src/pages/instrument/api/delete-instrument.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import {API_INSTRUMENT_BY_ID, MOCK_SERVER_URL} from "shared/config/backend";
+import Jwt from "domain/model/jwt";
+import {InstrumentId} from "@domain/model/instrument-id";
+
+interface DeleteInstrumentByIdResponse {
+    status: number;
+}
+
+export const deleteInstrument = async (id: InstrumentId): Promise<DeleteInstrumentByIdResponse> => {
+    const url = `${MOCK_SERVER_URL}${API_INSTRUMENT_BY_ID}${id.toStringValue()}/delete`;
+
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${Jwt.extractFromLocalStorage()?.toStringValue()}`,
+    }
+
+    const { status } = await axios.post(url, {
+        headers: headers,
+    });
+
+    return {
+        status: status,
+    };
+}
