63b44d051fd4d7eeb8051d277b6b6038b529f44c
skywalker <baskirill.an@gmail.com>
2024-07-28 16:29:43 +0300

    fix(eslint): try to fix error at CI

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,10 +1,12 @@
-import { browser } from "globals";
+import globals from "globals";
+
+const { browser } = globals.browser;
 import { configs } from "@eslint/js";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: browser } },
   configs.recommended,
-  ...configs.recommended,
-  configs.flat.recommended,
+  // ...configs.recommended,
+  // configs.flat.recommended
 ];
