407101ed159e6f60a2549ccbb9b304e70d5da9cc
skywalker <baskirill.an@gmail.com>
2024-07-28 13:33:05 +0300

    fix(linter): training commas at eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,17 +3,16 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
   },
   {
     languageOptions: {
-      globals: globals.browser
-    }
+      globals: globals.browser,
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended
+  pluginReact.configs.flat.recommended,
 ];
