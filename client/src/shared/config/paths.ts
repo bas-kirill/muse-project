d64c704bb4d99ae51d3e3bab4af55b350cfc0fe6
skywalker <baskirill.an@gmail.com>
2024-07-30 00:38:59 +0300

    chore(add-instrument): skeleton functional component and use static typing for frontend URIs

---
+++ client/src/shared/config/paths.ts
@@ -0,0 +1,7 @@
+export const HOME = "/";
+export const PROFILE = "/profile";
+export const CATALOGUE = "/catalogue";
+export const LOGIN = "/login";
+export const INSTRUMENT_BY_ID = "/instrument/:instrumentId";
+export const CREATE_INSTRUMENT = "/instrument/create";
+export const NOT_FOUND = "*";
