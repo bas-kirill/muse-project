4f61c84992d3155196257fec28796cb2f9fbf7c2
skywalker <baskirill.an@gmail.com>
2024-07-30 01:16:10 +0300

    feat(instrument-types): add `GET /api/instrument/types` endpoint to retrieve instrument types

---
+++ server/app/src/main/kotlin/mu/muse/rest/instruments/GetInstrumentTypesEndpoint.kt
@@ -0,0 +1,18 @@
+package mu.muse.rest.instruments
+
+import mu.muse.domain.instrument.Instrument
+import mu.muse.rest.API_GET_INSTRUMENT_TYPES
+import mu.muse.usecase.GetInstrumentTypes
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GetInstrumentTypesEndpoint(
+    private val getInstrumentTypes: GetInstrumentTypes,
+) {
+
+    @GetMapping(API_GET_INSTRUMENT_TYPES)
+    fun getInstrumentTypes(): List<Instrument.Type> {
+        return getInstrumentTypes.execute()
+    }
+}
