a3cd3f5b4d87358b7763e92ddacad4e354faf3b5
skywalker <baskirill.an@gmail.com>
2024-07-30 22:28:57 +0300

    chore(prettier): auto format

--- client/src/pages/log-in/index.ts
+++ client/src/pages/log-in/index.ts
@@ -2,8 +2,4 @@ import { LogIn } from "./ui/LogIn";
 import { action } from "./api/action";
 import { useJwt } from "./model/use-jwt";
 
-export {
-  LogIn,
-  action,
-  useJwt,
-};
+export { LogIn, action, useJwt };
