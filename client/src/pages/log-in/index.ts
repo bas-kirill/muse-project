1f8286723cfea3f92a379be8eda985b0068e8e9d
skywalker <baskirill.an@gmail.com>
2024-07-30 22:13:20 +0300

    feat(server): check jwt at home pages

--- client/src/pages/log-in/index.ts
+++ client/src/pages/log-in/index.ts
@@ -1,4 +1,9 @@
 import { LogIn } from "./ui/LogIn";
 import { action } from "./api/action";
+import { useJwt } from "./model/use-jwt";
 
-export { LogIn, action };
+export {
+  LogIn,
+  action,
+  useJwt,
+};
