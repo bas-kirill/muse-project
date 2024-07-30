541e64660229279341999111b7ffe0c97f8dfb4b
skywalker <baskirill.an@gmail.com>
2024-07-30 21:12:27 +0300

    feat(edit-instrument): form to edit instrument without server logic

--- client/src/domain/model/instrument.ts
+++ client/src/domain/model/instrument.ts
@@ -1,12 +1,20 @@
+import { InstrumentName } from "domain/model/instrument-name";
+import { InstrumentType } from "domain/model/instrument-type";
+import { ManufacturerName } from "domain/model/manufacturer-name";
+import { ManufactureDate } from "domain/model/manufacture-date";
+import { ReleaseDate } from "domain/model/release-date";
+import { Country } from "domain/model/country";
+import { Materials } from "domain/model/material";
+
 export interface Instrument {
   id: number;
-  name: string;
-  type: string;
-  manufacturer: string;
-  manufacturerDate: string;
-  releaseDate: string;
-  country: string;
-  basicMaterials: string[];
+  name: InstrumentName;
+  type: InstrumentType;
+  manufacturer: ManufacturerName;
+  manufacturerDate: ManufactureDate;
+  releaseDate: ReleaseDate;
+  country: Country;
+  basicMaterials: Materials;
 }
 
 export type Instruments = Instrument[];
--- client/src/pages/edit-instrument/ui/EditInstrument.tsx
+++ client/src/pages/edit-instrument/ui/EditInstrument.tsx
@@ -1,28 +1,32 @@
 import React from "react";
-import "./CreateInstrument.css";
+import "./EditInstrument.css";
 import { Header } from "widgets/header";
 import { Footer } from "widgets/footer";
 import { Form, useActionData, useLoaderData } from "react-router-dom";
-import { CreateInstrumentLoader } from "pages/create-instrument";
-import { CreateInstrumentAction } from "pages/create-instrument/api/action";
+import { EditInstrumentLoader } from "pages/edit-instrument/api/loader";
+import { EditInstrumentAction } from "pages/edit-instrument/api/action";
 
-export const CreateInstrument = () => {
-  const loader = useLoaderData() as CreateInstrumentLoader;
-  const actionData = useActionData() as CreateInstrumentAction;
+export const EditInstrument = () => {
+  const loader = useLoaderData() as EditInstrumentLoader;
+  const actionData = useActionData() as EditInstrumentAction;
 
   return (
     <>
       <Header />
-      <h1>Instrument Creation</h1>
-      <Form method="POST" id="create-instrument">
-        <div className="create-instrument-field">
-          <label htmlFor="instrument-name">Instrument Name</label>
-          <input type="text" name="instrument-name" required />
+      <h1>Edit Instrument</h1>
+      <Form method="POST" id="edit-instrument">
+        <div className="edit-instrument-field">
+          <input type="hidden" name="instrument-id" value={loader.instrumentForEdit.id} />
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
+          <label htmlFor="instrument-name">Instrument Name</label>
+          <input type="text" name="instrument-name" defaultValue={loader.instrumentForEdit.name} required />
+        </div>
+
+        <div className="edit-instrument-field">
           <label htmlFor="instrument-type">Instrument Type</label>
-          <select name="instrument-type" required>
+          <select name="instrument-type" defaultValue={loader.instrumentForEdit.type} required>
             {loader.instrumentTypes.map((instrumentType) => (
               <option key={instrumentType} value={instrumentType}>
                 {instrumentType}
@@ -31,9 +35,9 @@ export const CreateInstrument = () => {
           </select>
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
           <label htmlFor="manufacturer-name">Manufacturer name</label>
-          <select name="manufacturer-name" required>
+          <select name="manufacturer-name" defaultValue={loader.instrumentForEdit.name} required>
             {loader.manufacturerNames.map((manufacturerName) => (
               <option key={manufacturerName} value={manufacturerName}>
                 {manufacturerName}
@@ -42,31 +46,33 @@ export const CreateInstrument = () => {
           </select>
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
           <label htmlFor="manufacturer-date">Manufacturer date</label>
           <input
             type="date"
             name="manufacturer-date"
             min="0001-01-01"
             max="9999-12-31"
+            defaultValue={loader.instrumentForEdit.manufacturerDate}
             required
           />
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
           <label htmlFor="release-date">Release date</label>
           <input
             type="date"
             name="release-date"
             min="0001-01-01"
             max="9999-12-31"
+            defaultValue={loader.instrumentForEdit.releaseDate}
             required
           />
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
           <label htmlFor="country">Country</label>
-          <select name="country" required>
+          <select name="country" defaultValue={loader.instrumentForEdit.country} required>
             {loader.countries.map((country) => (
               <option key={country} value={country}>
                 {country}
@@ -75,9 +81,9 @@ export const CreateInstrument = () => {
           </select>
         </div>
 
-        <div className="create-instrument-field">
+        <div className="edit-instrument-field">
           <label htmlFor="material">Basic Material</label>
-          <select name="material" required>
+          <select name="material" defaultValue={loader.instrumentForEdit.basicMaterials} required>
             {loader.materials.map((material) => (
               <option key={material} value={material}>
                 {material}
@@ -86,7 +92,7 @@ export const CreateInstrument = () => {
           </select>
         </div>
 
-        <input type="submit" value="Create Instrument" />
+        <input type="submit" value="Edit Instrument" />
       </Form>
       {actionData?.errors &&
         actionData?.errors.map((error) => <div key={error}>{error}</div>)}
