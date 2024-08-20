9b353ff9a0568330ac242fb3305b859d14103333
skywalker <baskirill.an@gmail.com>
2024-08-20 19:41:09 +0300

    chore(edit-instrument): linter

--- client/src/pages/create-instrument/api/action.ts
+++ client/src/pages/create-instrument/api/action.ts
@@ -1,29 +1,12 @@
 import { ActionFunction } from "react-router-dom";
 import axios from "axios";
 import { SERVER_URL } from "shared/config";
-import { InstrumentName } from "domain/model/instrument-name";
-import { InstrumentType } from "domain/model/instrument-type";
-import { ManufacturerName } from "domain/model/manufacturer-name";
-import { ManufactureDate } from "domain/model/manufacture-date";
-import { ReleaseDate } from "domain/model/release-date";
-import { Country } from "domain/model/country";
-import { Materials } from "domain/model/material";
 import { API_CREATE_INSTRUMENT } from "shared/config/backend";
 import Jwt from "domain/model/jwt";
 import { parseInstrumentDetails } from "shared/model/parseInstrumentDetails";
 
 export interface CreateInstrumentAction {
   errors: string[] | null;
-}
-
-interface CreateInstrumentRequestBody {
-  instrumentName: InstrumentName;
-  instrumentType: InstrumentType;
-  manufacturerName: ManufacturerName;
-  manufactureDate: ManufactureDate;
-  releaseDate: ReleaseDate;
-  country: Country;
-  materials: Materials;
 }
 
 export const action: ActionFunction = async ({
@@ -46,19 +29,17 @@ export const action: ActionFunction = async ({
     };
   }
 
-  const createInstrumentRequestBody = {
-    instrumentName: instrumentName,
-    instrumentType: instrumentType,
-    manufacturerName: manufacturerName,
-    manufactureDate: manufactureDate,
-    releaseDate: releaseDate,
-    country: country,
-    material: materials,
-  };
-
   const { status } = await axios.post(
     `${SERVER_URL}${API_CREATE_INSTRUMENT}`,
-    createInstrumentRequestBody,
+    {
+      instrumentName: instrumentName,
+      instrumentType: instrumentType,
+      manufacturerName: manufacturerName,
+      manufactureDate: manufactureDate,
+      releaseDate: releaseDate,
+      country: country,
+      material: materials,
+    },
     {
       headers: {
         Authorization: `Bearer ${Jwt.extractFromLocalStorage()?.toStringValue()}`,
