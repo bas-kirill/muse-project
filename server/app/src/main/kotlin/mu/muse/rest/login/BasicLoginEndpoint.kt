ac37d5d7e221978049e9e71b0f3bb44d4bd75275
skywalker <baskirill.an@gmail.com>
2024-08-21 14:18:08 +0300

    chore(static-analyzer): run it

--- server/app/src/main/kotlin/mu/muse/rest/login/BasicLoginEndpoint.kt
+++ server/app/src/main/kotlin/mu/muse/rest/login/BasicLoginEndpoint.kt
@@ -16,14 +16,21 @@ import org.springframework.web.context.request.ServletRequestAttributes
 @RestController
 class BasicLoginEndpoint(private val basicLogin: BasicLogin) : BasicLoginApi {
 
-    override fun basicLogin(usernameAndPasswordRequestBody: UsernameAndPasswordRequestBody): ResponseEntity<JwtResponse> {
+    companion object {
+        const val COOKIE_MAX_AGE_SEVEN_DAYS_IN_SECONDS = 7 * 24 * 60 * 60
+        const val COOKIE_PATH = "/"
+    }
+
+
+    override fun basicLogin(usernameAndPasswordRequestBody: UsernameAndPasswordRequestBody):
+        ResponseEntity<JwtResponse> {
         val id = Username.from(usernameAndPasswordRequestBody.username)
         val password = Password.from(usernameAndPasswordRequestBody.password)
         val jwtRaw = basicLogin.execute(id, password)
         val cookie = Cookie("jwt", jwtRaw)
         cookie.isHttpOnly = false // because we need to extract a role from token at client side
-        cookie.maxAge = 7 * 24 * 60 * 60  // 7 days
-        cookie.path = "/"
+        cookie.maxAge = COOKIE_MAX_AGE_SEVEN_DAYS_IN_SECONDS  // 7 days
+        cookie.path = COOKIE_PATH
         (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).response?.addCookie(cookie)
         return jwtRaw.toResponseEntity()
     }
