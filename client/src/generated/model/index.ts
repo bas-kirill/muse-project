c699d8022c960cdf8cf8d081149b93f0823d9ef9
skywalker <baskirill.an@gmail.com>
2024-08-21 23:11:06 +0300

    chore(openapi-generator): commit autogenerated typescript code for future

---
+++ client/src/generated/model/index.ts
@@ -0,0 +1,7 @@
+export * from "./get-instrument-by-criteria-page-response";
+export * from "./get-instrument-criteria-request-body";
+export * from "./get-instruments-by-criteria-response";
+export * from "./instrument-detail";
+export * from "./jwt-response";
+export * from "./profile-details-response";
+export * from "./username-and-password-request-body";
