f570cfadef11027422841446f38a69b78b2bcee9
skywalker <baskirill.an@gmail.com>
2024-08-22 19:25:55 +0300

    chore(openapi): remove `-it`

--- tools/scripts/openapi/runLinter.sh
+++ tools/scripts/openapi/runLinter.sh
@@ -5,6 +5,5 @@ rootDir="$currentDir/../../../"
 
 (cd "$rootDir/openapi" && exec docker run \
   --rm \
-  -it \
   -v "$(pwd):/tmp" \
   stoplight/spectral:6.11.1 lint --ruleset "/tmp/.spectral.yml" "/tmp/specs/**/*.yml")
