a92ad50a15de98da04bfaa4f9a27c5a36fd0621d
skywalker <baskirill.an@gmail.com>
2024-08-22 19:23:10 +0300

    chore(openapi): use docker container to run `spectral` linter

--- tools/scripts/openapi/runLinter.sh
+++ tools/scripts/openapi/runLinter.sh
@@ -3,4 +3,8 @@ set -e
 currentDir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 rootDir="$currentDir/../../../"
 
-(cd "$rootDir/openapi" && exec spectral lint ./specs/**/*.yml)
+(cd "$rootDir/openapi" && exec docker run \
+  --rm \
+  -it \
+  -v "$(pwd):/tmp" \
+  stoplight/spectral:6.11.1 lint --ruleset "/tmp/.spectral.yml" "/tmp/specs/**/*.yml")
