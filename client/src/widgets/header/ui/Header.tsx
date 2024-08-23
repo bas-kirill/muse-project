2f52b61bfbf33f6189e15bdf29440950213663ce
skywalker <baskirill.an@gmail.com>
2024-08-23 21:59:11 +0300

    feat(flexible-design): introduce flexible design and redesign home page

--- client/src/widgets/header/ui/Header.tsx
+++ client/src/widgets/header/ui/Header.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { CATALOGUE, FAVORITE, HOME, LOGIN, PROFILE } from "shared/config/paths";
 import { Cookies } from "typescript-cookie";
 import { Jwt } from "domain/model/jwt";
@@ -8,33 +8,31 @@ import { Role } from "domain/model/role";
 
 export function Header() {
   const jwt = useRef<string | undefined>(
-    Cookies.get("jwt") as string | undefined,
+    Cookies.get("jwt") as string | undefined
   );
+
+  const navigate = useNavigate();
 
   return (
     <header>
       <nav>
+        <button onClick={_ => navigate(HOME)}>Home</button>
+        <button onClick={_ => navigate(CATALOGUE)}>Catalogue</button>
+        <button onClick={_ => navigate(FAVORITE)}>Favorite</button>
+
+
         {jwt.current === undefined && (
-          <div>
-            <Link to={LOGIN}>Log in</Link>
-          </div>
-        )}
+          <button onClick={_ => navigate(LOGIN)}>Login</button>
+        )
+        }
 
         {jwt.current !== undefined &&
           Jwt.from(jwt.current).toRole() === Role.Editor && (
-            <div>
-              <Link to={PROFILE}>Profile</Link>
-            </div>
+            <button onClick={_ => navigate(PROFILE)}>
+              Profile
+            </button>
           )}
-        <div>
-          <Link to={FAVORITE}>Favorite</Link>
-        </div>
-        <div>
-          <Link to={CATALOGUE}>Catalogue</Link>
-        </div>
-        <div>
-          <Link to={HOME}>Home</Link>
-        </div>
+
       </nav>
     </header>
   );
