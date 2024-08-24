df7ed4b65f2ac53ce4517a727a258f5a5f357621
skywalker <baskirill.an@gmail.com>
2024-08-24 17:22:46 +0300

    fix(postgres): issues with running at docker

--- server/app/src/main/kotlin/mu/muse/application/Environment.kt
+++ server/app/src/main/kotlin/mu/muse/application/Environment.kt
@@ -1,8 +1,4 @@
 // @formatter: off
 
-val POSTGRES_USER_ENV = System.getenv("POSTGRES_USER") ?: throw RuntimeException("`POSTGRES_USER` env not found")
-val POSTGRES_PASSWORD_ENV = System.getenv("POSTGRES_PASSWORD") ?: throw RuntimeException("`POSTGRES_PASSWORD` env not found")
-val POSTGRES_DB = System.getenv("POSTGRES_DB") ?: throw RuntimeException("`POSTGRES_DB` env not found")
-val SERVER_JDBC_URL_ENV = System.getenv("POSTGRES_DSN") ?: throw RuntimeException("`SERVER_JDBC_URL` env not found")
 
 // @formatter: on
--- server/app/src/main/kotlin/mu/muse/application/muse/PersistenceConfiguration.kt
+++ server/app/src/main/kotlin/mu/muse/application/muse/PersistenceConfiguration.kt
@@ -1,15 +1,12 @@
 package mu.muse.application.muse
 
-import POSTGRES_DB
-import POSTGRES_PASSWORD_ENV
-import POSTGRES_USER_ENV
-import SERVER_JDBC_URL_ENV
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import mu.muse.persistence.instrument.postgres.PostgresInstrumentIdGenerator
 import mu.muse.persistence.instrument.postgres.PostgresInstrumentRepository
 import mu.muse.persistence.user.postgres.PostgresUserIdGenerator
 import mu.muse.persistence.user.postgres.PostgresUserRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
@@ -18,13 +15,29 @@ import javax.sql.DataSource
 @Configuration
 class PersistenceConfiguration {
 
+//    val POSTGRES_USER_ENV = System.getenv("POSTGRES_USER") ?: throw RuntimeException("`POSTGRES_USER` env not found")
+//    val POSTGRES_PASSWORD_ENV = System.getenv("POSTGRES_PASSWORD") ?: throw RuntimeException("`POSTGRES_PASSWORD` env not found")
+//    val POSTGRES_DB = System.getenv("POSTGRES_DB") ?: throw RuntimeException("`POSTGRES_DB` env not found")
+//    val SERVER_JDBC_URL_ENV = System.getenv("POSTGRES_DSN") ?: throw RuntimeException("`SERVER_JDBC_URL` env not found")
+
+
+    @Value("\${POSTGRES_USER}")
+    private lateinit var postgresUser: String
+
+    @Value("\${POSTGRES_PASSWORD}")
+    private lateinit var postgresPassword: String
+
+    @Value("\${POSTGRES_DSN}")
+    private lateinit var postgresDsn: String
+
+
     @Bean
     fun dataSource(): DataSource {
         val hikariConfig = HikariConfig()
-        hikariConfig.username = POSTGRES_USER_ENV
-        hikariConfig.password = POSTGRES_PASSWORD_ENV
+        hikariConfig.username = postgresUser
+        hikariConfig.password = postgresPassword
         hikariConfig.driverClassName = "org.postgresql.Driver"
-        hikariConfig.jdbcUrl = SERVER_JDBC_URL_ENV
+        hikariConfig.jdbcUrl = postgresDsn
         val hikariDatasource = HikariDataSource(hikariConfig)
         // 25 is a good enough data pool size, it is a database in a container after all
         hikariDatasource.maximumPoolSize = 25
