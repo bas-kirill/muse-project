45f44332f245c4f7b26869b336c50631927959e6
skywalker <baskirill.an@gmail.com>
2024-08-28 19:58:39 +0300

    fix(dev): use `--verbose`

--- tools/scripts/openapi/regenerateOpenApi.sh
+++ tools/scripts/openapi/regenerateOpenApi.sh
@@ -14,8 +14,10 @@ rootDir="$currentDir/../../../"
     --input-spec /local/openapi/openapi.yml \
     --output /local/client/src/generated \
     --generator-name typescript-axios \
-    --additional-properties=apiPackage=api,modelPackage=model,supportsES6=true,withSeparateModelsAndApi=true
+    --additional-properties=apiPackage=api,modelPackage=model,supportsES6=true,withSeparateModelsAndApi=true \
+    --verbose
 )
+
 (cd "$rootDir/server" && ./gradlew clean)
 (cd "$rootDir/server" && ./gradlew openApiGenerate)
 (cd "$rootDir" && ./tools/scripts/client/runLinter.sh)
