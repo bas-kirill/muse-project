afe2edb0fe008524e575bc0df27fe3e1a5d7c9f3
skywalker <baskirill.an@gmail.com>
2024-08-28 20:08:24 +0300

    fix(dev): mkdir folder

--- tools/scripts/openapi/regenerateOpenApi.sh
+++ tools/scripts/openapi/regenerateOpenApi.sh
@@ -5,17 +5,19 @@ rootDir="$currentDir/../../../"
 
 (cd "$rootDir" && exec ./tools/scripts/openapi/bundle.sh)
 (cd "$rootDir/client" && rm -rf ./src/generated)
-(cd "$rootDir" && exec docker run \
-    --rm \
-    -v "${PWD}/openapi:/local/openapi" \
-    -v "${PWD}/client/src/generated:/local/client/src/generated" \
-    -u "$(id -u)":"$(id -g)" \
-    openapitools/openapi-generator-cli:v7.8.0 generate \
-    --input-spec local/openapi/openapi.yml \
-    --output local/client/src/generated \
-    --generator-name typescript-axios \
-    --additional-properties=apiPackage=api,modelPackage=model,supportsES6=true,withSeparateModelsAndApi=true \
-    --verbose
+(cd "$rootDir" &&
+  mkdir local &&
+  docker run \
+  --rm \
+  -v "${PWD}/openapi:/local/openapi" \
+  -v "${PWD}/client/src/generated:/local/client/src/generated" \
+  -u "$(id -u)":"$(id -g)" \
+  openapitools/openapi-generator-cli:v7.8.0 generate \
+  --input-spec local/openapi/openapi.yml \
+  --output local/client/src/generated \
+  --generator-name typescript-axios \
+  --additional-properties=apiPackage=api,modelPackage=model,supportsES6=true,withSeparateModelsAndApi=true \
+  --verbose
 )
 
 (cd "$rootDir/server" && ./gradlew clean)
