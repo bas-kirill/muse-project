85de15990b78aa8933e4a8fa044cbb0a61a1be5d
skywalker <baskirill.an@gmail.com>
2024-08-28 16:52:18 +0300

    chore(openapi-generator): commit codegen

---
+++ server/app/build/openapi/src/main/kotlin/mu/muse/rest/dto/ManufactureDate.kt
@@ -0,0 +1,26 @@
+package mu.muse.rest.dto
+
+import java.util.Objects
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.DecimalMax
+import jakarta.validation.constraints.DecimalMin
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
+import jakarta.validation.Valid
+
+/**
+ * 
+ * @param manufactureDate 
+ */
+data class ManufactureDate(
+
+    @field:Valid
+    @get:JsonProperty("manufacture_date", required = true) val manufactureDate: java.time.LocalDate
+    ) {
+
+}
+
